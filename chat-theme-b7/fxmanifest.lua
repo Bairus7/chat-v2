5446cc28982053b02e90944b119b32fada0ab577
Bairus7 <55885536+Bairus7@users.noreply.github.com>
2021-01-18 14:29:51 +0100

    Add files via upload

---
+++ chat-theme-b7/fxmanifest.lua
@@ -0,0 +1,13 @@
+file 'style.css'
+file 'shadow.js'
+
+chat_theme 'b7' {
+    styleSheet = 'style.css',
+    msgTemplates = {
+        default = '<b>{0}</b><span>{1}</span>'
+    }
+}
+
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
